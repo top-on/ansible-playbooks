46d5e12a1b8fa69c978fa81878b244ada314d62b
top-on <top-on@posteo.de>
2022-08-27 00:02:10 +0200

    firefox privacy settings

--- config/user.js
+++ config/user.js
@@ -1,2 +1,17 @@
 // firefox config file, set by ansible
 user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.contentblocking.category", "strict");
+
+user_pref("privacy.clearOnShutdown.cache", true);	
+user_pref("privacy.clearOnShutdown.cookies", true);	
+user_pref("privacy.clearOnShutdown.downloads", true);	
+user_pref("privacy.clearOnShutdown.formdata", true);	
+user_pref("privacy.clearOnShutdown.history", true);	
+user_pref("privacy.clearOnShutdown.sessions", true);
+
+
+user_pref("privacy.clearOnShutdown.openWindows", true)
+user_pref("privacy.clearOnShutdown.siteSettings", true)
+user_pref("privacy.clearsitedata.cache.enabled", true)
+user_pref("privacy.donottrackheader.enabled", true)
+user_pref("privacy.sanitize.sanitizeOnShutdown", true)
